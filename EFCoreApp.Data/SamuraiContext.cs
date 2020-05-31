210db940278bc98e95ccf973ac437b1003899ade
Vladimir <vladkodoktorov@gmail.com>
2020-05-31 16:53:36 +0100

    added migration + UI

--- EFCoreApp.Data/SamuraiContext.cs
+++ EFCoreApp.Data/SamuraiContext.cs
@@ -11,9 +11,14 @@ namespace EFCoreApp.Data
     public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
-
         public DbSet<Quote> Quotes { get; set; }
-
         public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                 "Server = (localdb)\\mssqllocaldb; Database = EFCoreApp; Trusted_Connection = True; ");
+        }
+
     }
 }
