ba077264c738803f9a54da95fc9dbbd11530d59e
Vladimir <vladkodoktorov@gmail.com>
2020-06-04 08:40:34 +0100

    added logger + packages

--- EFCoreApp.Data/SamuraiContext.cs
+++ EFCoreApp.Data/SamuraiContext.cs
@@ -5,24 +5,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EFCoreApp.Data
 {
     public class SamuraiContext : DbContext
     {
+
+        private ILoggerFactory MyConsoleLoggerFactory;
+
+        public SamuraiContext()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder
+                        .AddConsole()
+                        .AddFilter
+                        (DbLoggerCategory.Database.Command.Name, LogLevel.Information));
+
+            MyConsoleLoggerFactory = serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        }
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
+            optionsBuilder
+                .UseLoggerFactory(MyConsoleLoggerFactory)
+                .EnableSensitiveDataLogging(true)
+                .UseSqlServer(
                  "Server = (localdb)\\mssqllocaldb; Database = EFCoreApp; Trusted_Connection = True; ");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>().HasKey(x => new { x.SamuraiId, x.BattleId });
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
