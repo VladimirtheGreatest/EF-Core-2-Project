ccc9ef7ce4af488e5b5690ee21331574cc85de4b
Vladimir <vladkodoktorov@gmail.com>
2020-06-04 08:53:04 +0100

    added data

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EFCoreApp.Data;
+using EFCoreApp.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,29 @@ namespace UI
     {
         static void Main(string[] args)
         {
+            InsertSamurai();
+            InsertMultiple();
+        }     
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Vladimir" };
+            using(var context = new SamuraiContext())
+            {
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
+        }
+        private static void InsertMultiple()
+        {
+            var samurai = new Samurai { Name = "Vladimir" };
+            var samurai2 = new Samurai { Name = "John" };
+            //for each loop to generate many users and insert them into database??
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.AddRange(samurai, samurai2);
+                context.SaveChanges();
+            }
         }
     }
 }
