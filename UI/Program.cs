31a8b0b53c47a34faaf2b730aea9a5af6fba8046
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2020-06-05 14:53:30 +0100

    Update Program.cs

--- UI/Program.cs
+++ UI/Program.cs
@@ -17,7 +17,7 @@ namespace UI
         }     
         //adding some extra content for masters branch
         //this is not going to appear on development
-        //testing merging
+        //creating conflict from githubo from my develop branch
         private static void InsertSamurai()
         {
             var samurai = new Samurai { Name = "Vladimir" };
